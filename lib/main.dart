87463d0fcdc3c312e52ac9fbb210eb648144a2f4
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2021-07-21 20:07:59 +0700

    add scaffold stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
       ),
+      backgroundColor: Colors.blue,
+      body: Container(),
     );
+  }
+}
