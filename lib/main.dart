6a454384e03fb3e1badb07b9f3c330e96fdd7aa5
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2021-07-22 18:51:05 +0700

    randomize the ball number

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -28,10 +29,20 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print(ballNumber);
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
